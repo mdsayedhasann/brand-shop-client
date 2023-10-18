8f2cb6408b18d6c5db814fb8cea906d8dfd506d1
sayed_hossain <sayed.hasan0011@gmail.com>
2023-10-19 01:33:27 +0600

    Basic header and Footer

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,17 @@
 
 import { Outlet } from 'react-router-dom';
+import Header from '../layouts/Header';
+import Footer from '../layouts/Footer';
 
 const Root = () => {
     return (
+        <>
+        <div className='max-w-full mx-auto'>
+        <Header></Header>
         <Outlet></Outlet>
+        <Footer></Footer>
+        </div>
+        </>
     );
 };
 
